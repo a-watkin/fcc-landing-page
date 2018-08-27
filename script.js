d15206d9b2577bb31450c08a9acd91f917874007
adam <atomicpenguines@gmail.com>
2018-08-27 05:07:37 +0100

    before adding flexbox elements

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-    // tests the script is being loaded
-    alert('test');
+//     // tests the script is being loaded
+//     alert('test');
 
-    $.noConflict();
-    jQuery(document).ready(function($){
-       alert('jQuery is working');
-    });
+//     $.noConflict();
+//     jQuery(document).ready(function($){
+//        alert('jQuery is working');
+//     });
 
-});
+// });
